f1da45baa2d9908dbcb011ffe004df184cc7f7fb
Alfredo Hernandez <a1ahl611@gmail.com>
2023-02-22 17:31:05 -0600

    Added festival section

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+//css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+//img
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   //Identificar el archivo SASS
@@ -13,10 +19,43 @@ function css(done) {
   done(); //Callback que avisa a gulp que se llego al final.
 }
 
+function images(done) {
+  const options = {
+    optimizationLevel: 50
+  };
+  src("src/img/**/*{png,jpg}")
+    .pipe(cache(imagemin(options)))
+    .pipe(dest("build/img"))
+  done();
+}
+
+function versionWebp(done) {
+  const options = {
+    quality: 50
+  };
+  src("src/img/**/*{png,jpg}")
+    .pipe(webp(options))
+    .pipe(dest("build/img"))
+  done();
+}
+
+function versionAvif(done) {
+  const options = {
+    quality: 50
+  };
+  src("src/img/**/*{png,jpg}")
+    .pipe(avif(options))
+    .pipe(dest("build/img"))
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css)
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(images, versionWebp, versionAvif, dev );
