88b16fd179daf551b5eade063396fe231838a2df
Alfredo Hdz <a1ahl611@gmail.com>
2023-02-26 22:43:04 -0600

    Added more configs

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@ const { src, dest, watch, parallel } = require("gulp");
 //css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 //img
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
+//JS
+const terser = require("gulp-terser-js");
 
 function css(done) {
   //Identificar el archivo SASS
   //Compilarlo
   //Almacenarlo
   src("src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest("build/css"));
   done(); //Callback que avisa a gulp que se llego al final.
 }
@@ -51,6 +60,9 @@ function versionAvif(done) {
 
 function javascript(done) {
   src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'));
   done()
 }
